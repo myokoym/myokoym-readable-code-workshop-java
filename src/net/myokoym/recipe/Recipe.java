4b552d0c0838ac4f49f949cdf7c2c2c18fe62d09
myokoym <myokoym@gmail.com>
2015-03-06 00:10:25 +0900

    spec3: split data file

--- src/net/myokoym/recipe/Recipe.java
+++ src/net/myokoym/recipe/Recipe.java
@@ -1,9 +1,33 @@
 package net.myokoym.recipe;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Recipe {
 
 	public static void main(String[] args) {
-		System.out.println("オムライス");
-	}
+		String fileName = args[0];
+		File file = new File(fileName);
+		FileReader fileReader = null;
+		try {
+			fileReader = new FileReader(file);
+		} catch (FileNotFoundException e) {
+			System.err.println("ファイルのオープンに失敗しました: <" + fileName + ">");
+			System.exit(1);		
+		}
+		
+		String recipe = null;
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		try {
+			recipe = bufferedReader.readLine();
+		} catch (IOException e) {
+			System.err.println("ファイルの読み込みに失敗しました: <" + fileName + ">");
+			System.exit(1);		
+		}
 
+		System.out.println(recipe);
+	}
 }
